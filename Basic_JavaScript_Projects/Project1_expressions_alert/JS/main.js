c5d8f85491567c40b85f74fa860e0e9b221f8e55
Zakkarte <dakotabkent@gmail.com>
2022-05-17 11:25:03 -0400

    as per instructions

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function() {                                          //Defining a function and naming it
+    var str = "This is the button text!";                               //Defining a variable and giving it
+    document.getElementById("Button_Text").innerHTML = str;             //Putting the value
+}
+
