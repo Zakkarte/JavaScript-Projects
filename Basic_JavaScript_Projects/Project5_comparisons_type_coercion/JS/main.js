00b25486afca989f0abae346cfeb778cc514e140
Zakkarte <dakotabkent@gmail.com>
2022-05-18 08:37:00 -0400

    as per instructions

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,44 @@
+function my_Function() {    //Function is not functional unless one of the below options is un-commented
+//    document.getElementById("Test").innerHTML = 0/0
+//    document.getElementById("Test").innerHTML = isNaN('This is a string');
+//    document.getElementById("Test").innerHTML = isNaN('007')
+//    document.write(-3E310)      //Display -Infinity
+//    document.write(3E310)       //Display Infinity
+}
+
+
+//    document.write ("83" + 10);   //Adding a sting to a numerical value 
+
+//    document.write (typeof "Word");     //Displays the data type in this case "String"
+//    document.write (typeof 3);      //Displays the data type in this case "Number"
+
+//    document.write (10 > 8)  //True
+//    document.write (10 < 8)  //False
+
+//    console.log (3 + 4)      //Displays a math output within dev tools in webpage
+
+//    console.log (10 > 12)    //Displays a false output in console in dev tools
+
+//    document.write(10 == 10)       //True
+
+//    document.write(10 == 12)       //False
+
+// var X = 82
+// var Y = 82
+// var Z = "82"
+// var T = "String"
+
+//    document.write(X === Y)     //True        
+//    document.write(X === T)     //False
+//    document.write(X === Z)     //False
+//    document.write(Z === T)     //False
+
+//    document.write (5 > 2 && 10 >4);    //True
+//    document.write (5 > 10 && 10 > 4);  //False
+//    document.write (5 > 10 || 10 > 4);  //True
+//    document.write (5 > 10 || 10 > 20); //False
+
+function not_Function() {       //Function is not functional unless one of the below options is un-commented
+//    document.getElementById("Not").innerHTML = !(20 > 10);  //False
+//    document.getElementById("Not").innerHTML = !(5 > 10);   //True
+}
